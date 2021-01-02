d26db4ac032bf8918dc76e483499ddc5d3d21f3d
Junjinjen <kapibaranelol@gmail.com>
2021-01-02 21:55:33 +0300

    Add project files.

---
+++ Firmware/Firmware.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Firmware.ino
+ Created:	1/2/2021 9:48:35 PM
+ Author:	Junjinjen
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
